21f34d19db8084397a6c01609c767b5f41b1aa42
Deden Fahrul <113590217+uapap@users.noreply.github.com>
2025-07-04 15:01:03 +0700

    update tool panel

--- src/App.js
+++ src/App.js
@@ -171,6 +171,14 @@ function App() {
 		);
 	};
 
+	const downloadCVFromTools = () => {
+		// Open Google Drive CV link in new tab (Tools Panel)
+		window.open(
+			"https://drive.google.com/file/d/1lo37QbMMkfeCUDVK1592tOeCCe2g0D6y/view?usp=drive_link",
+			"_blank"
+		);
+	};
+
 	const toggleMobileMenu = () => {
 		setIsMobileMenuOpen(!isMobileMenuOpen);
 	};
@@ -707,12 +715,14 @@ function App() {
 				</div>
 				<div
 					className="tool-item"
-					data-tooltip="Download CV"
-					onClick={downloadCV}>
+					data-tooltip="Download Portfolio Project"
+					onClick={downloadCVFromTools}>
 					<svg viewBox="0 0 24 24">
-						<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-						<polyline points="7,10 12,15 17,10" />
-						<line x1="12" y1="15" x2="12" y2="3" />
+						<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+						<polyline points="14,2 14,8 20,8" />
+						<line x1="16" y1="13" x2="8" y2="13" />
+						<line x1="16" y1="17" x2="8" y2="17" />
+						<polyline points="10,9 9,9 8,9" />
 					</svg>
 				</div>
 			</div>
